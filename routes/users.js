31e715f86109454c12384e6b00c53d0964b5b5af
AnthonyAtencio <draco159@gmail.com>
2021-10-23 09:19:16 -0500

    Se añade modulo "uuid" para creación de IDs unicos.
    Se añade el metodo delete y post para la ruta users.

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,31 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
-const router= express.Router();
+const router = express.Router();
 
-const users= [
-    {
-        firstName: "Juanito",
-        lastName: "Perez",
-        age: 20    
-    },
-    {
-        firstName: "Laura",
-        lastName: "Rodriguez",
-        age: 20    
-    }
-];
+let users = [];
 
-router.get('/',(req, res) =>{
+router.get('/', (req, res) => {
     res.send(users);
 });
 
-router.post('/',(req,res)=>{
-    const newUser=req.body;
-    users.push(newUser);
+router.post('/', (req, res) => {
+    const newUser = req.body;
+    users.push({ ...newUser, id: uuidv4() });
     res.send(`The user ${newUser.firstName} has been added to the database.`);
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    const foundUser=users.find((user) => user.id === id);
+    console.log(foundUser);
+    res.send(foundUser);
+});
+
+router.delete('/:id',(req,res)=>{
+    const {id} = req.params;
+    users=users.filter((user)=>user.id !==id);
+    res.send(`The user with ID ${id} has been deleted from the database.`);
 });
 
 export default router;
